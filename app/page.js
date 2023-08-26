81325569375cca233dc25bf61601afdd46e9673d
AmanChoudhary2020 <amanch@umich.edu>
2023-08-25 23:11:43 -0400

    finished projects section, fixed dark mode issues

--- app/page.js
+++ app/page.js
@@ -21,12 +21,6 @@ import 'react-slideshow-image/dist/styles.css'
 import trashbots from "../public/trashbots.jpg";
 import criteo from "../public/criteo.png";
 import deepgram from "../public/deepgram.png";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -88,7 +82,7 @@ export default function Home() {
               Developer and designer.
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 mx-auto md:text-xl">
-              Computer Science Student @ <a href="https://umich.edu/" className="text-teal-500">University of Michigan</a>
+              Computer Science Student @ <a href="https://umich.edu/" target="_blank" className="text-teal-500">University of Michigan</a>
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <a href="mailto:amanch@umich.edu" target="_blank"><AiTwotoneMail /></a>
@@ -120,14 +114,14 @@ export default function Home() {
               University of Michigan &apos;24, B.S.E in Computer Science and Engineering
             </h3> */}
 
-            <p className="text-md leading-5 pb-2 text-gray-800 dark:text-gray-200">
+            <p className="text-md leading-6 pb-2 text-gray-800 dark:text-gray-200">
               Born and raised in the heart of Silicon Valley, I am an aspiring software
               developer seeking full-time software engineering roles in a fast-growing
               and exciting environment. I have interest and experience full stack development,
               data engineering, and machine learning.
             </p>
-            <p>In my free time, I enjoy playing guitar and
-              shooting photography &#x1F4F7;. Check out my guitar solo in a <a href="https://www.youtube.com/watch?v=CQI5q4grxjA&t=88s" target="_blank" className="text-teal-500 hover:underline">tune</a> my buddies and I wrote!</p>
+            <p className="dark:text-gray-200">In my free time, I enjoy playing guitar and
+              shooting photography &#x1F4F7;. Check out my guitar solo on a <a href="https://www.youtube.com/watch?v=CQI5q4grxjA&t=88s" target="_blank" className="text-teal-500 hover:underline">tune</a> my buddies and I wrote!</p>
           </div>
 
           {/* WORK EXPERIENCE */}
@@ -136,13 +130,13 @@ export default function Home() {
           </h3>
 
           <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-5 rounded-xl my-25 dark:bg-white flex-1">
+            <a href="https://deepgram.com/" target="_blank" className="dark:border-solid dark:border-slate-50 dark:border-2 dark:bg-opacity-0 dark:text-white hover:scale-105 text-center p-5 shadow-lg rounded-xl dark:bg-white flex-1">
               <div className="mx-auto w-20 h-20">
-                <a href="https://deepgram.com/" target="_blank"><Image src={deepgram}
-                  className="rounded-lg object-cover hover:scale-105 drop-shadow-2xl"
+                <Image src={deepgram}
+                  className="rounded-lg object-cover"
                   width={"100%"}
                   height={"100%"}
-                  layout="responsive" /></a>
+                  layout="responsive" />
               </div>
               <h3 className="text-lg font-medium pt-8">Deepgram</h3>
               <p className="py-2">
@@ -151,14 +145,14 @@ export default function Home() {
               <h3>Worked on core automatic speech recognition engine, radically increasing confidence
                 in new releases and researching GPU runtime optimizations
               </h3>
-            </div>
-            <div className="text-center shadow-lg p-5 rounded-xl dark:bg-white flex-1">
+            </a>
+            <a href="https://www.criteo.com/" target="_blank" className="dark:border-solid dark:border-slate-50 dark:border-2 dark:bg-opacity-0 dark:text-white hover:scale-105 text-center p-5 shadow-lg rounded-xl dark:bg-white flex-1">
               <div className="mx-auto pt-6 w-20 h-20">
-                <a href="https://www.criteo.com/" target="_blank"><Image src={criteo}
-                  className="rounded-lg object-cover hover:scale-105 drop-shadow-md"
+                <Image src={criteo}
+                  className="rounded-lg object-cover"
                   width={"100%"}
                   height={"100%"}
-                  layout="responsive" /></a>
+                  layout="responsive" />
               </div>
               <h3 className="text-lg font-medium pt-8">
                 Criteo
@@ -170,14 +164,14 @@ export default function Home() {
                 Developed Spark pipelines for processing large-scale retail media advertising event
                 datasets on an hourly basis
               </h3>
-            </div>
-            <div className="text-center shadow-lg p-5 rounded-xl my-25 dark:bg-white flex-1">
-              <div className="mx-auto w-20 h-20">
-                <a href="https://trashbots.co/" target="_blank"><Image src={trashbots}
-                  className="rounded-lg object-cover hover:scale-105 drop-shadow-md"
+            </a>
+            <a href="https://www.trashbots.co/" target="_blank" className="dark:border-solid dark:border-slate-50 dark:border-2 dark:bg-opacity-0 dark:text-white hover:scale-105 text-center p-5 shadow-lg rounded-xl dark:bg-white flex-1">
+              <div className="mx-auto pt-6 w-20 h-20">
+                <Image src={trashbots}
+                  className="rounded-lg object-cover"
                   width={"100%"}
                   height={"100%"}
-                  layout="responsive" /></a>
+                  layout="responsive" />
               </div>
               <h3 className="text-lg font-medium pt-8">
                 Trashbots Co.
@@ -189,63 +183,83 @@ export default function Home() {
                 Integrated new features on web-based application used to interact with robot,
                 contributed to lower-level firmware and bluetooth functionality
               </h3>
-            </div>
+            </a>
           </div>
         </section>
-        <section id="Projects" className="py-10">
+        <section id="Projects" className="py-10 dark:text-gray-200">
           <h3 className="text-3xl pb-5 py-1">Projects</h3>
           <div className="grid grid-cols-3 gap-10">
-            <a className="group rounded-md text-center"
-              href="https://github.com/AmanChoudhary2020/nn-from-scratch" target="_blank">
-              <img class="group-hover:opacity-10 rounded-lg object-cover shadow-lg "
-                width={"100%"}
-                height={"100%"}
-                src="nn.jpg" />
-              {/* <div className="transition-all transform -translate-y-60 opacity-0 group-hover:opacity-100 group-hover:-translate-y-100">
-                <p className="pb-4">Building Neural Nets from Scratch</p>
-                <p>
-                  A tiny scalar-valued autograd engine from scratch with Rust, inspired by Andrej Karpathy's micrograd project
-                </p>
-              </div> */}
-            </a>
-            <a className="group text-center border-none rounded-md"
-              href="https://github.com/AmanChoudhary2020/News-Engagement" target="_blank">
-              <img class="group-hover:opacity-10 rounded-lg object-cover shadow-lg "
-                width={"100%"}
-                height={"100%"}
-                src="engagement.jpg" />
-              {/* <div className="transition-all transform -translate-y-60 opacity-0 group-hover:opacity-100 group-hover:-translate-y-100">
-                <p className="pb-4">
-                  Reading Beyond the Headline: Identifying and Understanding News Reader Feedback
-                </p>
-                <p>
-                  In-depth linguistic analysis of questions and comments posed to news stories on social media,
-                  with the goal of helping news organizations anticipate the information needs of their audience
-                </p>
-              </div> */}
-            </a>
-            <a className="group text-center border-none rounded-md"
-              href="#">
-              <img class="group-hover:opacity-10 rounded-lg object-cover shadow-lg "
-                width={"100%"}
-                height={"100%"}
-                src="nn.jpg" />
-              <div className="transition-all transform -translate-y-60 opacity-0 group-hover:opacity-100 group-hover:-translate-y-100">
-                <p className="pb-4">A scalable search engine</p>
-                <p>
-                  Built using PageRank-based ranking system, Hadoop Streaming to create a segmented inverted index, and a distributed system for search
-                </p>
+            <a href="https://github.com/AmanChoudhary2020/News-Engagement" target="_blank" class="w-100 h-min relative group">
+              <img src="engagement.jpg" height={"40%"} className="rounded-lg group-hover:opacity-10" />
+              <div class="opacity-0 group-hover:opacity-100 duration-300 text-center absolute inset-x-0 top-40 flex justify-center items-end">
+                <div>
+                  <p className="font-bold leading-5 pb-1">Predicting News Reader Feedback with Deep Learning</p>
+                  <p>In-depth linguistic analysis of questions and comments posed to news stories
+                    on social media, with the goal of helping news organizations anticipate the
+                    information needs of their audience
+                  </p>
+                </div>
               </div>
             </a>
+
+            <a href="https://github.com/AmanChoudhary2020/search-engine" target="_blank" class="w-100 h-min relative group">
+              <img src="nn.jpg" className="object-fill h-85 w-full rounded-lg group-hover:opacity-10 flex-1" />
+              <div class="opacity-0 group-hover:opacity-100 duration-300 text-center absolute inset-x-0 top-40 flex justify-center items-end">
+                <div>
+                  <p className="font-bold leading-5 pb-1">Scalable Search Engine</p>
+                  <p>Built a scalable search engine using PageRank-based ranking system, Hadoop
+                    Streaming to create a segmented inverted index, and a distributed system for
+                    search
+                  </p>
+                </div>
+              </div>
+            </a>
+
+            <a href="https://github.com/AmanChoudhary2020/search-engine" target="_blank" class="w-100 h-min relative group">
+              <img src="engine.jpg" className="object-fill h-85 w-full rounded-lg group-hover:opacity-10 flex-1" />
+              <div class="opacity-0 group-hover:opacity-100 duration-300 text-center absolute inset-x-0 top-40 flex justify-center items-end">
+                <div>
+                  <p className="font-bold leading-5 pb-1">Scalable Search Engine</p>
+                  <p>Built a scalable search engine using PageRank-based ranking system, Hadoop
+                    Streaming to create a segmented inverted index, and a distributed system for
+                    search
+                  </p>
+                </div>
+              </div>
+            </a>
+
+
+            {/* <a className="text-center flex-1"
+              href="https://github.com/AmanChoudhary2020/nn-from-scratch" target="_blank">
+              <div className="w-100 relative group">
+                <img src="engagement.jpg" width={"100%"}
+                  height={"100%"} className="group-hover:opacity-10 rounded-lg shadow-medium" />
+                <div className="opacity-0 group-hover:opacity-100 duration-300 absolute inset-x-0 bottom-20 items-center flex justify-center">
+                  A tiny scalar-valued autograd engine from scratch with Rust, inspired by Andrej Karpathy's micrograd project
+                </div>
+              </div>
+            </a>
+
+            <a className="text-center flex-1"
+              href="https://github.com/AmanChoudhary2020/nn-from-scratch" target="_blank">
+              <div className="w-100 relative group">
+                <img src="engine.jpg" width={"100%"}
+                  height={"100%"} className="group-hover:opacity-10 rounded-lg shadow-medium" />
+                <div className="opacity-0 group-hover:opacity-100 duration-300 absolute inset-x-0 bottom-30 items-center flex justify-center">
+                  A tiny scalar-valued autograd engine from scratch with Rust, inspired by Andrej Karpathy's micrograd project
+                </div>
+              </div>
+            </a> */}
           </div>
 
         </section>
-        <section id="Contact" className="py-10 text-center">
+        <section id="Contact" className="dark:text-gray-200 py-10 text-center">
           <h3 className="text-3xl pb-5 py-1">Let&apos;s Chat!</h3>
           <h3 className="text-m">amanch@umich.edu</h3>
           <h3 className="text-m">408-913-0300</h3>
+          <h3 className="text-m">https://www.linkedin.com/in/aman-ch/</h3>
         </section>
-        <footer className="relative h-100 w-100">
+        <footer className="relative h-100 w-100 dark:text-gray-200">
           <p className="text-xs pb-1 absolute bottom-0 right-0">Author: Aman Choudhary</p>
         </footer>
       </main >
